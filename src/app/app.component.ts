10b83bec6cbac8460686c57ef261b2fccbe93de1
Ranaman196 <rana_ehtisham@hotmail.com>
2020-12-04 21:07:26 +0500

    pipe tutorial completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'masteringPipes';
+  name: string;
+  date: string;
+  amount: number;
+  height: number;
+  miles: number;
+
+  car = {
+    make: "BMW",
+    model: "Roadster",
+    year: "2020"
+  }
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value)
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value)
+  }
+
+  onInputChange(value: string) {
+    this.name = value
+  }
+
+  onDateChange(value: string) {
+    this.date = value;
+  }
+
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
 }
---
+++ src/app/convert.pipe.ts
@@ -0,0 +1,28 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert'
+})
+export class ConvertPipe implements PipeTransform {
+
+  transform(value: any, targetUnits: string): any {
+    if (!value) {
+      return '';
+    }
+
+    switch (targetUnits) {
+      case 'km':
+        return value * 1.609;
+      case 'm':
+        return value * 1.609 * 1000;
+      case 'cm':
+        return value * 1.609 * 1000 * 1000;
+      default:
+        throw new Error('Target unit not supported')
+    }
+
+    return value * 1.609;
+  }
+
+
+}
